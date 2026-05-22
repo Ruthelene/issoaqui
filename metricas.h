c861ff6f99a3a8b552cf2c6392675f162b6a621b
Ruthelene <ruthelene.farias@gmail.com>
2026-05-22 18:03:01 -0400

    Update metricas.h

--- metricas.h
+++ metricas.h
@@ -4,19 +4,17 @@
 #include <stdio.h>
 #include <time.h>
 
-/* Retorna tempo atual em segundos com precisão de nanosegundos. */
 double metricasAgora(void);
 
-/* Calcula a média de um vetor de doubles de tamanho n. */
 double metricasMedia(double *tempos, int n);
 
-/* Imprime tabela simples: índice | tempo (s) */
+
 void metricasImprimirTabela(const char *titulo,
                              double     *tempos,
                              int         n,
                              double      media);
 
-/* Retorna uso de memória RSS do processo em KB (-1 se indisponível). */
+
 long metricasMemoriaKB(void);
 
-#endif /* METRICAS_H */
+#endif 
