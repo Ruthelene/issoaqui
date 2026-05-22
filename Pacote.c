b079bea3e77de53786ea2c5e3b3beaba669e7866
Ruthelene <ruthelene.farias@gmail.com>
2026-05-22 18:03:34 -0400

    Update comments and formatting in Pacote.c
    Fix formatting and comments in pacoteImprimirEmOrdem function.

--- Pacote.c
+++ Pacote.c
@@ -20,7 +20,7 @@ NoPacote *pacoteInserir(NoPacote *raiz, int id, const char *dado) {
     return raiz;
 }
 
-/* Imprime todos os pacotes em ordem crescente de ID (caminhamento central). */
+
 void pacoteImprimirEmOrdem(NoPacote *raiz) {
     if (!raiz) return;
     pacoteImprimirEmOrdem(raiz->esq);
